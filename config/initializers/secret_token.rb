68b6be363ca661d2a71b51f6ddbe4a6dee3f5dfa
Manuel Albarran <manu@manutosh.(none)>
2012-10-30 14:45:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiwitime::Application.config.secret_token = 'f5e1edc2ea3463e4d09b0cd8f8fc460804b4f139e15b1a559d95200118f4361e7945541cb2c5b9f921dcd1242143ff06ee818b28fc2c22fbbc7c157aa70ecdbe'
