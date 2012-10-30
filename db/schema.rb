061cf582e148369fa9c6f45a56ba0f0e89d1f48e
Manuel <manu@batkiwi.com>
2012-10-30 23:46:52 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030170320) do
+ActiveRecord::Schema.define(:version => 20121030224012) do
 
   create_table "users", :force => true do |t|
-    t.string   "provider"
-    t.string   "uid"
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
