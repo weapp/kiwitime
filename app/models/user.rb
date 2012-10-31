552ba9bd2c769c50c071e4d7667826a8b088f42b
Manuel <manu@batkiwi.com>
2012-10-31 14:00:40 +0100

    Added some validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
-  validates :name, length: { maximum: 140 }
+  validates :name, {length: {maximum: 140} , presence: true}
+  validates :email, presence: true
 end
