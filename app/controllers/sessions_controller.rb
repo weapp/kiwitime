6f90d9ee4e1c8ac2f63e282aa79f6674bebc5310
Manuel <manu@batkiwi.com>
2012-11-03 11:46:54 +0100

    Added Session controller methods and helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticcate(params[:session][:email],
+                              params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid/email password combination."
+      render 'new'
+    else
+      sing_in user
+      redirect_to user
+    end
   end
 
   def destroy
