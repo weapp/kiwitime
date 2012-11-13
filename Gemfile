5bbc69adb82f58edc4a148d757d8c898908a52d3
Manuel <manu@batkiwi.com>
2012-11-13 15:23:47 +0100

    Added includes to queries to improve the performance

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ end
 
 gem 'jquery-rails'
 
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.includes(:tasks).all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
-    @project = Project.find(params[:id])
+    @project = Project.includes(tasks: {sittings: {user: {}}}).find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
