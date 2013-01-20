e664b0f99821f0a5a39fc91f8ab87c05dff5f978
Manuel <manu@batkiwi.com>
2013-01-20 19:52:59 +0100

    Simplify charts on Sprint

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -28,27 +28,27 @@ class Sprint < ActiveRecord::Base
     @total_points ||= (tasks.collect{|t| t.points||0}).sum
   end
 
-  def chart
-    return @chart if @chart
-    data_table = GoogleVisualr::DataTable.new
-    # Add Column Headers 
-    data_table.new_column('string', 'Day' ) 
-    data_table.new_column('number', 'Actual') 
-    data_table.new_column('number', 'Scope')
-    data = (init..finish).collect do |d|
+  def stats
+  	@stats ||= (init..finish).collect do |d|
       [
         d.to_s(:short),
         (Time.now >= d) ? (total_points - tasks.select{|t| t.finished_at d }.collect{|t| t.points}.sum) : nil,
         (total_points * (finish - d) / days),
       ]
-    end
+  	end
+  end
 
-    
-    if data[0][1].present?
-      # Add Rows and Values 
-      data_table.add_rows(data)
-      option = { width: 800, height: 400, title: to_s, legend: {position: "none"} }
-      @chart = GoogleVisualr::Interactive::LineChart.new(data_table, option)
+  def chart
+    if stats[0][1].present?
+    data_table = GoogleVisualr::DataTable.new
+    # Add Column Headers 
+    data_table.new_column('string', 'Day' ) 
+    data_table.new_column('number', 'Actual') 
+    data_table.new_column('number', 'Scope')
+    # Add Rows and Values 
+    data_table.add_rows(stats)
+    option = { width: 800, height: 400, title: to_s, legend: {position: "none"} }
+    @chart = GoogleVisualr::Interactive::LineChart.new(data_table, option)
     end
   end
 
