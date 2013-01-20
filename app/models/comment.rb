57bdbde80c6a26d9341055de50cfbd1d1a887d34
Manuel <manu@batkiwi.com>
2013-01-20 18:38:28 +0100

    Annotated comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  project_id :integer
+#  task_id    :integer
+#  message    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
   attr_accessible :end, :start, :task_id, :user_id, :day, :message
 
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  project_id :integer
+#  task_id    :integer
+#  message    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
